e48bdecea191a4d5de32d4a6e03f964814c9cb2a
dave-wu-dev-12 <davidwwu12@gmail.com>
2021-03-09 22:13:11 -0600

    resp ehader

--- spotify-clone/src/components/Header.js
+++ spotify-clone/src/components/Header.js
@@ -2,18 +2,18 @@ import React from "react";
 import "./Header.css";
 import AccountCircleIcon from "@material-ui/icons/AccountCircle";
 import KeyboardArrowDownIcon from "@material-ui/icons/KeyboardArrowDown";
+import MenuIcon from "@material-ui/icons/Menu";
 
 function Header() {
   return (
     <div className="header_outer_container">
       <div className="header_container">
-        <div className="header_logo">
-          <img
-            className="header_logo_image"
-            src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/26/Spotify_logo_with_text.svg/1200px-Spotify_logo_with_text.svg.png"
-            alt="logo.png"
-          />
-        </div>
+        <img
+          className="header_logo_image"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/26/Spotify_logo_with_text.svg/1200px-Spotify_logo_with_text.svg.png"
+          alt="logo.png"
+        />
+        {/* desktop header */}
         <div className="header_actions">
           <p>Premium</p>
           <p>Support</p>
@@ -25,6 +25,16 @@ function Header() {
             <KeyboardArrowDownIcon />
           </div>
         </div>
+        {/* desktop header */}
+
+        {/* mobile header */}
+        <div className="header_actions_mobile">
+          <div className="action_profile_container">
+            <AccountCircleIcon className="margin_right_qtr" />
+            <MenuIcon />
+          </div>
+        </div>
+        {/* mobile header */}
       </div>
     </div>
   );
