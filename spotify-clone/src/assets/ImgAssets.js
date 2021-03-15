b832bc0b5ac9cc02d7790ec0ebf9119fdad77aec
dave-wu-dev-12 <davidwwu12@gmail.com>
2021-03-14 20:13:27 -0500

    adding recently added section with header updates

---
+++ spotify-clone/src/assets/ImgAssets.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const ImgAssets = {
+  albumImgFutureSrc:
+    "https://static.independent.co.uk/s3fs-public/thumbnails/image/2020/05/15/10/screen-shot-2020-05-15-at-10.34.50.png?width=982&height=726&auto=webp&quality=75",
+  albumImgWeekndSrc:
+    "https://consequenceofsound.net/wp-content/uploads/2020/03/weeknd-after-hours-deluxe-artwork.jpeg?quality=80&w=807",
+  albumImgPrmreSrc:
+    "https://i.pinimg.com/originals/db/17/43/db17432e7b0125c6de59d9f8500eb4ca.jpg",
+  albumImgGeazySrc:
+    "https://i.pinimg.com/736x/38/44/33/3844334ed78c191f3f7c9afeccd54d40.jpg",
+  albumImgZeddSrc:
+    "https://upload.wikimedia.org/wikipedia/en/3/32/Zedd_-_Clarity_%28album%29.png",
+  albumImgSelenagSrc:
+    "https://mtv.mtvnimages.com/uri/mgid:ao:image:mtv.com:105097?quality=0.8&format=jpg&width=1440&height=810&.jpg",
+};
+
+export default ImgAssets;
--- spotify-clone/src/components/RecentlyPlayed.js
+++ spotify-clone/src/components/RecentlyPlayed.js
@@ -1,8 +1,53 @@
 import React from "react";
 import "./RecentlyPlayed.css";
+import ArrowDropDownCircleIcon from "@material-ui/icons/ArrowDropDownCircle";
+import ImgAssets from "../assets/ImgAssets";
 
 function RecentlyPlayed() {
-  return <div></div>;
+  return (
+    <div className="recentlyPlayed_outerContainer">
+      <div className="recentlyPlayed_contentContainer">
+        <div className="recentlyPlayed_playerContainer">
+          <div className="jumpBackIn_container">
+            <p>Recently Played</p>
+            <h1>Jump back in</h1>
+            <h5>Pick up your music right where you left off.</h5>
+            <button className="webPlayer_button">Open Web player</button>
+          </div>
+          <div className="playlistGrid_Container">
+            <div className="playlistAlbumContainer">
+              <img src={ImgAssets.albumImgFutureSrc} alt="album" />
+              <span className="albumOverlay">FUTURE</span>
+            </div>
+            <div className="playlistAlbumContainer">
+              <img src={ImgAssets.albumImgGeazySrc} alt="album" />
+              <span className="albumOverlay">G-EAZY</span>
+            </div>
+            <div className="playlistAlbumContainer">
+              <img src={ImgAssets.albumImgPrmreSrc} alt="album" />
+              <span className="albumOverlay">PARAMORE</span>
+            </div>
+            <div className="playlistAlbumContainer">
+              <img src={ImgAssets.albumImgSelenagSrc} alt="album" />
+              <span className="albumOverlay">SELENA GOMER</span>
+            </div>
+            <div className="playlistAlbumContainer">
+              <img src={ImgAssets.albumImgZeddSrc} alt="album" />
+              <span className="albumOverlay">ZEDD</span>
+            </div>
+            <div className="playlistAlbumContainer">
+              <img src={ImgAssets.albumImgWeekndSrc} alt="album" />
+              <span className="albumOverlay">THE WEEKND</span>
+            </div>
+          </div>
+        </div>
+        <div className="recentlyPlayed_nextContianer">
+          <ArrowDropDownCircleIcon />
+        </div>
+      </div>
+      <div className="banner__fade"></div>
+    </div>
+  );
 }
 
 export default RecentlyPlayed;
