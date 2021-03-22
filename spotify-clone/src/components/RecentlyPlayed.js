892058d2ed3395c2c17e968a447039c9a1ccbdc5
dave-wu-dev-12 <davidwwu12@gmail.com>
2021-03-21 20:43:45 -0500

    updating animatinos on recently played

--- spotify-clone/src/components/RecentlyPlayed.js
+++ spotify-clone/src/components/RecentlyPlayed.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./RecentlyPlayed.css";
 import ArrowDropDownCircleIcon from "@material-ui/icons/ArrowDropDownCircle";
 import ImgAssets from "../assets/ImgAssets";
+import PlayCircleOutlineIcon from "@material-ui/icons/PlayCircleOutline";
 
 function RecentlyPlayed() {
   return (
@@ -17,32 +18,59 @@ function RecentlyPlayed() {
           <div className="playlistGrid_Container">
             <div className="playlistAlbumContainer">
               <img src={ImgAssets.albumImgFutureSrc} alt="album" />
-              <span className="albumOverlay">FUTURE</span>
+              <span className="albumOverlay">
+                <PlayCircleOutlineIcon />
+                <span className="albumOverlay_artistName">FUTURE</span>
+                <span className="albumOverlay_title">WIZRUD</span>
+              </span>
             </div>
             <div className="playlistAlbumContainer">
               <img src={ImgAssets.albumImgGeazySrc} alt="album" />
-              <span className="albumOverlay">G-EAZY</span>
+              <span className="albumOverlay">
+                <PlayCircleOutlineIcon />
+                <span className="albumOverlay_artistName">G-EAZY</span>
+                <span className="albumOverlay_title">TGIF</span>
+              </span>
             </div>
             <div className="playlistAlbumContainer">
               <img src={ImgAssets.albumImgPrmreSrc} alt="album" />
-              <span className="albumOverlay">PARAMORE</span>
+              <span className="albumOverlay">
+                <PlayCircleOutlineIcon />
+                <span className="albumOverlay_artistName">PARAMORE</span>
+                <span className="albumOverlay_title">rockCity</span>
+              </span>
             </div>
             <div className="playlistAlbumContainer">
               <img src={ImgAssets.albumImgSelenagSrc} alt="album" />
-              <span className="albumOverlay">SELENA GOMER</span>
+              <span className="albumOverlay">
+                <PlayCircleOutlineIcon />
+                <span className="albumOverlay_artistName">SELENA GOMER</span>
+                <span className="albumOverlay_title">My Bieber</span>
+              </span>
             </div>
             <div className="playlistAlbumContainer">
               <img src={ImgAssets.albumImgZeddSrc} alt="album" />
-              <span className="albumOverlay">ZEDD</span>
+              <span className="albumOverlay">
+                <PlayCircleOutlineIcon />
+                <span className="albumOverlay_artistName">ZEDD</span>
+                <span className="albumOverlay_title">
+                  Clarity of the Darkest night in the land of darksied the one
+                  ruler
+                </span>
+              </span>
             </div>
             <div className="playlistAlbumContainer">
               <img src={ImgAssets.albumImgWeekndSrc} alt="album" />
-              <span className="albumOverlay">THE WEEKND</span>
+              <span className="albumOverlay">
+                <PlayCircleOutlineIcon />
+                <span className="albumOverlay_artistName">THE WEEKND</span>
+                <span className="albumOverlay_title">TA hours</span>
+              </span>
             </div>
           </div>
         </div>
         <div className="recentlyPlayed_nextContianer">
-          <ArrowDropDownCircleIcon />
+          <ArrowDropDownCircleIcon className="hoverInfinite" />
         </div>
       </div>
       <div className="banner__fade"></div>
