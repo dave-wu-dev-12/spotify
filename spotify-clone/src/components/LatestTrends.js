34578d2abeb70535fabbeb270296ef7b7fab0866
dave-wu-dev-12 <davidwwu12@gmail.com>
2021-03-30 21:50:28 -0500

    beta layout wrap up

--- spotify-clone/src/components/LatestTrends.js
+++ spotify-clone/src/components/LatestTrends.js
@@ -1,8 +1,35 @@
 import React from "react";
 import "./LatestTrends.css";
+import ImgAssets from "../assets/ImgAssets";
 
 function LatestTrends() {
-  return <div></div>;
+  return (
+    <div className="contentOuterContainer">
+      <div className="contentInnerContainer">
+        <div className="latestTrendContainer">
+          <h2 className="latestTrendingHeader">Your latest listening trends</h2>
+          <div className="albumRow">
+            <div className="albumContainer">
+              <div className="albumInfo">
+                <span className="albumInfoMiniHeader">Top artists</span>
+                <h3 className="albumInfoMiniTitle">Kings of Leon</h3>
+                <p className="albumInfoMiniPlayNow">Play Now</p>
+              </div>
+              <img src={ImgAssets.albumImgPrmreSrc} alt="album" />
+            </div>
+            <div className="albumContainer">
+              <div className="albumInfo">
+                <span className="albumInfoMiniHeader">Top Track</span>
+                <h3 className="albumInfoMiniTitle">Baddies ONLY</h3>
+                <p className="albumInfoMiniPlayNow">Play Now</p>
+              </div>
+              <img src={ImgAssets.albumImgFutureSrc} alt="album" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default LatestTrends;
